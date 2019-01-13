21c2204bb829ca9344268d6565decb7935a8bd07
Martin Buchmann <Martin.Buchmann@googlemail.com>
2019-01-13 15:00:04 +0100

    Fälschlicherweise etwas gelöscht.

--- greetings.lisp
+++ greetings.lisp
@@ -4,7 +4,3 @@
     (loop for line = (read-line data nil nil)
           while line
           do (format t "~S" line))))
-
-(defmacro evaluator (s-exp)
-  `(format t "~A evalutes to: ~S" (write-to-string (quote ,s-exp)) ,s-exp))
-

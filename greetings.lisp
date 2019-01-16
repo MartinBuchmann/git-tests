86ac19aa8ae849463c3eb5bc801859e535756749
Martin Buchmann <Martin.Buchmann@googlemail.com>
2019-01-16 11:12:01 +0100

    Fixed a small issue with the line breaks.

--- greetings.lisp
+++ greetings.lisp
@@ -5,11 +5,11 @@
   (with-open-file (data file)
     (loop for line = (read-line data nil nil)
           while line
-          do (format t "~S" line))))
+          do (format t "~S~%" line))))
 
 (defmacro evaluator (s-exp)
   `(progn
-     (format t "~A evalutes to: ~S" (write-to-string (quote ,s-exp)) ,s-exp)
+     (format t "~A evalutes to: ~S~%" (write-to-string (quote ,s-exp)) ,s-exp)
      ,s-exp))
 
 (defun right-function ()
